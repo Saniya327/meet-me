82ab701154a40cc6cdc8cf96965ffeef204640b3
Saniya Farahath <saniyafarahath@Saniyas-Air.attlocal.net>
2023-09-10 15:26:27 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# meet-where
+
+meet-where is personal project for discovering a meeting place(restaurants, cafes) which is mid way between two locations.
+Ideal for first dates. Saves the trouble of looking at a map and searching for a place. 
+
+## libraries
+pip install requests
+
+Requires Google Maps API Key
+
+
+## Usage
+
+```python
+
+WIP
+
+
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
---
+++ places_api.py
@@ -0,0 +1,56 @@
+import json
+import gmaps
+import requests
+
+
+lat = 36.170216638996365
+long = -86.79001548571094
+x_diff = 0.5/69
+y_diff = 0.25/69
+
+high_lat = lat + x_diff
+high_long = long + y_diff
+low_lat = lat - x_diff
+low_long = long-y_diff
+
+
+url='https://places.googleapis.com/v1/places:searchText'
+headers = {
+    'Content-Type': 'application/json',
+    'X-Goog-Api-Key': 'your-api-key',
+    'X-Goog-FieldMask':'places.displayName,places.formattedAddress,places.priceLevel'
+}
+search_query = {
+   'textQuery' : 'Italian food',
+    "locationRestriction": {
+        "rectangle": {
+            "low": {
+            "latitude": low_lat,
+            "longitude": low_long
+            },
+            "high": {
+                "latitude":high_lat,
+                "longitude": high_long
+            }
+        }   
+    }
+}
+#locationRestriction
+
+#909 flats: 36.170216638996365, -86.79001548571094
+#high 36.180660668844496, -86.78828985350843
+#low 36.15894146751352, -86.79890165591115
+
+
+json_payload = json.dumps(search_query)
+
+response = requests.post(url, headers=headers, data=json_payload)
+
+if response.status_code == 200:
+    print('Request successful!')
+    print(response.json())
+else:
+    print('Request failed!')
+    print(response.text)
+
+
